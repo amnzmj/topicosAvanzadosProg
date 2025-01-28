3d39b9009bedb83ca5b6305ea7f3046a68077891
amnzmj <amunoz31965@ucq.edu.mx>
2025-01-28 16:07:19 -0600

    comments

--- ruleta.py
+++ ruleta.py
@@ -4,26 +4,26 @@ def ruleta_rusa(camaras=6, balas=1):
     if balas >= camaras:
         return "el maximo es 6"
     
-    # Creamos una lista que representa las cámaras del revólver
+    # de que capacidad el cargador
     cilindro = ["vacía"] * camaras
     
-    # Colocamos las balas en posiciones aleatorias
+    # random para las balas
     for _ in range(balas):
         posicion = random.randint(0, camaras - 1)
         while cilindro[posicion] == "bala":
-            posicion = random.randint(0, camaras - 1)  # Evitamos colocar dos balas en la misma cámara
+            posicion = random.randint(0, camaras - 1)  # no 2 balas en una camara
         cilindro[posicion] = "bala"
     
-    # El jugador elige una cámara al azar
+    
     eleccion_jugador = random.randint(0, camaras - 1)
     
-    # Comprobamos el resultado
+    # muerte?
     if cilindro[eleccion_jugador] == "bala":
         return "morido alv"
     else:
         return "no bala :( vivirás)"
 
-# Interacción con el usuario
+
 try:
     camaras = int(input("de que tamaño es el cilindro de tu revolver? (usualmente es de 6, luego hay de 5 o de 8)") or 6)
     balas = int(input("cuantas balas vas a meter en la recamara? (que tantas ganas de desvivirte)"))
